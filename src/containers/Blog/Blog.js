7f036c95a4d42891c63d69b01af498f6db6666bf
Faheem <faheemtech1@gmail.com>
2019-01-30 23:08:45 +0530

    preventing the reload for new links by use of Link component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+
 class Blog extends Component {
     render () {
                 return (
@@ -10,8 +12,12 @@ class Blog extends Component {
                 <header className="Blog">
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/" >Home</Link></li>
+                            <li><Link to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
@@ -19,6 +25,7 @@ class Blog extends Component {
                 <Route path="/" render= {() => <h1>Home 2</h1>} /> */}
 
                 <Route path="/" exact component={Posts} />
+                <Route path="/new-post" exact component={NewPost} />
             </div>
         );
     }
