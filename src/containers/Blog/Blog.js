d85c713b03f0e668798bfd2128b0cdd8dab404fe
Faheem <faheemtech1@gmail.com>
2019-01-30 22:40:49 +0530

    Route has been added for home and exact paths

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
+import { Route } from 'react-router-dom';
 import './Blog.css';
-import Posts from '../Posts/Posts';
 
 class Blog extends Component {
     render () {
@@ -15,7 +15,8 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                <Route path="/" exact render= {() => <h1>Home</h1>} />
+                <Route path="/" render= {() => <h1>Home 2</h1>} />
             </div>
         );
     }
