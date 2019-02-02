8ee794a9f67eb30825481e24ae60e8da9967702a
Faheem <faheemtech1@gmail.com>
2019-02-02 21:58:54 +0530

    react-router for POST and HOME

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+// import { withRouter } from 'react-router-dom';
 import './Post.css';
 
-const post = (props) => {
-    console.log(props);
-    return (
+const post = (props) => (
         <article className="Post" onClick={props.clicked}>
             <h1>{props.title}</h1>
             <div className="Info">
                 <div className="Author">{props.author}</div>
             </div>
-        </article>)
+        </article>
+        );
 
-};
-
-export default withRouter(post);
+// export default withRouter(post);
+export default post;
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
@@ -12,12 +12,12 @@ class Blog extends Component {
                 <header className="Blog">
                     <nav>
                         <ul>
-                            <li><Link to="/" >Home</Link></li>
-                            <li><Link to={{
+                            <li><NavLink to="/" exact activeClassName="my-active">Home</NavLink></li>
+                            <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
