1502131439726e56d69b6745552d37e2458f0576
Faheem <faheemtech1@gmail.com>
2019-02-02 23:19:38 +0530

    dynamic loading of post by params has been added

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,6 +4,7 @@ import { Route, NavLink } from "react-router-dom";
 import "./Blog.css";
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
+import FullPost from "./FullPost/FullPost";
 
 class Blog extends Component {
   render() {
@@ -13,10 +14,15 @@ class Blog extends Component {
           <nav>
             <ul>
               <li>
-                <NavLink to="/" exact activeClassName="my-active" activeStyle= {{
-                    color: '#fa923f',
-                    textDecoration: 'underline'
-                }}>
+                <NavLink
+                  to="/"
+                  exact
+                  activeClassName="my-active"
+                  activeStyle={{
+                    color: "#fa923f",
+                    textDecoration: "underline"
+                  }}
+                >
                   Home
                 </NavLink>
               </li>
@@ -39,6 +45,7 @@ class Blog extends Component {
 
         <Route path="/" exact component={Posts} />
         <Route path="/new-post" exact component={NewPost} />
+        <Route path="/:id" exact component={FullPost} />
       </div>
     );
   }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,53 +1,55 @@
-import React, { Component } from 'react';
-import axios from '../../../axios';
-import Post from '../../../components/Post/Post';
-import './Posts.css';
+import React, { Component } from "react";
+import axios from "../../../axios";
+import Post from "../../../components/Post/Post";
+import "./Posts.css";
+import { Link } from "react-router-dom";
+
 class Posts extends Component {
-    state = {
-        posts: []
-    }
-    componentDidMount () {
-        console.log(this.props);
-        axios.get( '/posts' )
-            .then( response => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Max'
-                    }
-                });
-                this.setState({posts: updatedPosts});
-                // console.log( response );
-            } )
-            .catch(error => {
-                console.log(error);
-                // this.setState({error: true});
-            });
-    }
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    console.log(this.props);
+    axios
+      .get("/posts")
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: "Max"
+          };
+        });
+        this.setState({ posts: updatedPosts });
+        // console.log( response );
+      })
+      .catch(error => {
+        console.log(error);
+        // this.setState({error: true});
+      });
+  }
 
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
-    }
-    render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post 
-                    key={post.id} 
-                    title={post.title} 
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)} />;
-            });
-        }
-
+  postSelectedHandler = id => {
+    this.setState({ selectedPostId: id });
+  };
+  render() {
+    let posts = <p style={{ textAlign: "center" }}>Something went wrong!</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
         return (
-            <section className="Posts">
-                    {posts}
-                </section>
+          <Link to={"/" + post.id} key={post.id}>
+            <Post
+              title={post.title}
+              author={post.author}
+              clicked={() => this.postSelectedHandler(post.id)}
+            />
+          </Link>
         );
+      });
     }
+
+    return <section className="Posts">{posts}</section>;
+  }
 }
 
 export default Posts;
